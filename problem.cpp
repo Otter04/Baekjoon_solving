b6d974e5d108ee5c5361cc37a23fdb0cb64ad2ab
Otter04 <145958988+Otter04@users.noreply.github.com>
2024-01-03 19:18:25 +0900

    first commit(cpp)

--- problem.cpp
+++ problem.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-print("Hello");
+class Account {
+	int balance;
+	string name;
+public:
+	Account() {
+		this->balance = balance;
+		this->name = name;
+	}
+	Account(int balance, string name) {
+		this->balance = balance;
+		this->name = name;
+	}
+	void setBal(int balance) {
+		this->balance = balance;
+	}
+	void setName(string name) {
+		this->name = name;
+	}
+	int getBal() {
+		return balance;
+	}
+	string getName() {
+		return name;
+	}
+	void getinformation() {
+		cout << "ÀÜ¾×: " << getBal() << ", ÀÌ¸§:" << getName() << endl;
+	}
+};
+
+int main() {
+	Account anc1(40,"Kang");
+	anc1.getBal();
+	anc1.getName();
+	anc1.getinformation();
 }
